4bac72802ada1db96e7ba01c09ac238e34ac7de4
Xander Hendry <vhendry.coding@gmail.com>
2023-09-06 11:53:14 -0400

    iteration 2

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
 class Student
+  attr_reader :name,
+              :age,
+              :scores
+  def initialize(student_info)
+    @name = student_info[:name]
+    @age = student_info[:age]
+    @scores = []
+  end
+
+  def log_score(score)
+    @scores << score
+  end
   
+  def grade
+    @scores.inject {|sum, score| (sum + score).to_f / @scores.length }
+  end
 end
--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -3,5 +3,34 @@ require './lib/course'
 require './lib/student'
 
 RSpec.describe Course do
-  
+  before(:each) do
+    @course =Course.new("Calculus", 2)
+    @student1 = Student.new({name: "Morgan", age: 21})
+    @student2 = Student.new({name: "Jordan", age: 29})
+  end
+  it 'exists' do
+    expect(@course).to be_instance_of(Course)
+  end
+
+  it 'has attributes' do
+    expect(@course.name).to eq("Calculus")
+    expect(@course.capacity).to eq(2)
+  end
+
+  it 'starts with no students' do
+    expect(@course.students).to eq([])
+  end
+
+  it 'can tell if it is at capacity' do
+    expect(@course.full?).to eq(false)
+    @course.enroll(@student1)
+    @course.enroll(@student2)
+    expect(@course.full?).to eq(true)
+  end
+
+  it 'can enroll students' do
+    @course.enroll(@student1)
+    @course.enroll(@student2)
+    expect(@course.students).to eq([@student1, @student2])
+  end
 end
